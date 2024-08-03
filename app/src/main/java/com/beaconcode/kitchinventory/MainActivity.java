8eb2ee648553757e501fc85d83acfd78908813fb
Neil Fachin <pipboy@gmail.com>
2024-08-02 17:50:15 -0700

    Create more activities and also fix menu bar so that it shows up on MainActivity.

--- app/src/main/java/com/beaconcode/kitchinventory/MainActivity.java
+++ app/src/main/java/com/beaconcode/kitchinventory/MainActivity.java
@@ -3,8 +3,11 @@ package com.beaconcode.kitchinventory;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -19,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.logout_menu, menu);
+        return true;
+    }
+
+
 
     static Intent mainActivityIntentFactory(Context context, int userId) {
         Intent intent = new Intent(context, MainActivity.class);
