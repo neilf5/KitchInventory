1a3cb871c19652db28c78d286e2ea6186d0ae81e
alexa <160264662+jalexasan@users.noreply.github.com>
2024-08-08 03:59:41 -0700

    Added logic for adding items from shopping list, not tested yet

--- app/src/main/java/com/beaconcode/kitchinventory/activities/KitchenAdd.java
+++ app/src/main/java/com/beaconcode/kitchinventory/activities/KitchenAdd.java
@@ -3,7 +3,9 @@ package com.beaconcode.kitchinventory.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toast;
 
 
 import androidx.activity.EdgeToEdge;
@@ -12,11 +14,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.beaconcode.kitchinventory.R;
+import com.beaconcode.kitchinventory.data.database.KitchenRepository;
+import com.beaconcode.kitchinventory.data.database.ShoppingListRepository;
+import com.beaconcode.kitchinventory.data.database.entities.Kitchen;
+import com.beaconcode.kitchinventory.data.database.entities.ShoppingList;
 import com.beaconcode.kitchinventory.databinding.ActivityKitchenAddBinding;
 
 public class KitchenAdd extends BaseActivity {
 
     private ActivityKitchenAddBinding binding;
+    private ShoppingListRepository shoppingListRepository;
+    private KitchenRepository kitchenRepository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,21 @@ public class KitchenAdd extends BaseActivity {
         setContentView(binding.getRoot());
 
         binding.addToKitchenButton.setOnClickListener(v -> {
+            String enteredFoodItem = binding.userShopListInput.getText().toString();
+
+            for (ShoppingList shoppinglist : shoppingListRepository.getAllShoppingList()) {
+                if (shoppinglist.getName().equals(enteredFoodItem) && getLoggedInUserId() == shoppinglist.getUserId()) {
+                    Toast.makeText(KitchenAdd.this, "Food found! Adding to your list!", Toast.LENGTH_SHORT).show();
+
+                    //add found food from shopping list over to kitchen's list
+                    Kitchen kitchen = new Kitchen(shoppinglist.getName(), shoppinglist.getQuantity(), shoppinglist.getUserId());
+                    kitchenRepository.insertKitchen(kitchen); //add new kitchen over to database
+                    shoppingListRepository.clearShoppingListByUserId(shoppinglist.getUserId()); //remove the item from shopping list
+                     return;
+                }
+
+                Toast.makeText(KitchenAdd.this, "Food item not found in your shopping list D:", Toast.LENGTH_SHORT).show();
+            }
 
         });
 
@@ -34,6 +57,13 @@ public class KitchenAdd extends BaseActivity {
         });
 
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.logout_menu, menu);
+        return true;
+    }
+
 
     static Intent kitchenAddActivityIntentFactory(Context context) {
         return new Intent(context, KitchenAdd.class);
